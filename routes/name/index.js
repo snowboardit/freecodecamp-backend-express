9307588fa94552f720f4518e9a85e0640591b252
Max Lareau <max@maxlareau.com>
2023-06-04 19:17:49 -0400

    Add '/name' POST route handler

--- routes/name/index.js
+++ routes/name/index.js
@@ -1,5 +1,15 @@
-function name(req, res) {
+const name = {}
+
+function get(req, res) {
     const { first, last } = req.query,
+        name = formatName(first, last)
+    res.status(200)
+        .json({ name })
+}
+
+function post(req, res) {
+    console.log(req.body)
+    const { first, last } = req.body,
         name = formatName(first, last)
     res.status(200)
         .json({ name })
@@ -8,5 +18,8 @@ function name(req, res) {
 function formatName(first, last) {
     return `${first} ${last}`
 }
+
+name.get = get;
+name.post = post
 
 module.exports = name
