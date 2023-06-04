8611cc3bebbbc4708977cda1550beb16815ff029
Max Lareau <max@maxlareau.com>
2023-06-04 14:41:35 -0400

    Fix static folder path

--- myApp.js
+++ myApp.js
@@ -1,7 +1,7 @@
 let express = require('express');
 let app = express();
 
-app.use(express.static('public'));
+app.use('/public', express.static('public'));
 
 app.get('/', (req, res) => {
     const file = __dirname + '/views/index.html'
